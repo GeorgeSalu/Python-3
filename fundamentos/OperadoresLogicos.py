542b2221cf112e08f45dd5d9f55a5af4bb47ef97
George Salu <george.salu10@gmail.com>
2018-12-14 13:15:18 -0200

    operadores logicos

---
+++ fundamentos/OperadoresLogicos.py
@@ -0,0 +1,20 @@
+print(True or False)
+print(7 != 3 and 2 > 3)
+
+# Tabela verdade do AND
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Tabela verdade do OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# Tabela verdade do XOR
+print(True is not True)
+print(True is not False)
+print(False is not True)
+print(False is not False)
