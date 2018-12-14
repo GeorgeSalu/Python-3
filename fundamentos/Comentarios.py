215ff1fb912485f6927eeb0d4aeaa4014a6cfacd
George Salu <george.salu10@gmail.com>
2018-12-14 11:45:30 -0200

    comentarios

---
+++ fundamentos/Comentarios.py
@@ -0,0 +1,17 @@
+# variaveis
+salario = 3460.45
+despesas = 2456.2
+
+"""
+A ideia e calcular o
+quanto vai sobrear no final do mes
+"""
+print(salario - despesas)
+
+# print('Fim')
+print('Fim de verdade')  # comentario aqui tb vale
+
+'''
+A ideia e calcular o
+quanto vai sobrear no final do mes
+'''
