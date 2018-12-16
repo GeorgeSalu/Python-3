04adfeae274765edef7084c3ae171f498fa32501
George Salu <george.salu10@gmail.com>
2018-12-16 18:45:23 -0200

    strings 01

---
+++ fundamentos/String01.py
@@ -0,0 +1,19 @@
+nome = 'Saulo Pedro'
+
+print(nome[0])
+print("Dias D'Avila" == 'Dias D\'Avila')
+
+texto = 'Texto entre apostrofe pode ter "aspas"'
+print(texto)
+
+doc = """
+texto com multiplas
+linhas
+"""
+print(doc)
+
+doc2 = """
+tambem e possivel com 
+3 aspas simples
+"""
+print(doc2)
