a6ed51dbd192efcc1fbbcd3d1fe8b7c7bbf2ef37
George Salu <george.salu10@gmail.com>
2018-12-16 18:55:15 -0200

    strings 02

---
+++ fundamentos/String02.py
@@ -0,0 +1,12 @@
+nome = 'Ana Paula'
+
+print(nome[0])
+print(nome[6])
+print(nome[-3])
+print(nome[4:])
+print(nome[-5:])
+
+numeros = '1234567890'
+print(numeros)
+print(numeros[::])
+print(numeros[::2])
