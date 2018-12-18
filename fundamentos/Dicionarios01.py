3690b61acea883be64c7cf13527c454e26d2ed3d
George Salu <george.salu10@gmail.com>
2018-12-18 19:15:11 -0200

    dicionarios 01

---
+++ fundamentos/Dicionarios01.py
@@ -0,0 +1,12 @@
+pessoa = {'nome': 'Prof Ana', 'idade': 38, 'cursos': ['Ingles', 'Portugues']}
+
+print(type(pessoa))
+print(len(pessoa))
+print(pessoa)
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+print(pessoa.get('idade'))
+print(pessoa.get('tags'))
