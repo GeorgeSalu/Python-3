1a0ac85c4526c0ac6449d7de52f69676b4af3545
George Salu <george.salu10@gmail.com>
2018-12-18 18:09:15 -0200

    listas 01

---
+++ fundamentos/Listas01.py
@@ -0,0 +1,14 @@
+lista = []
+
+print(type(lista))
+print(len(lista))
+
+lista.append(1)
+lista.append(2)
+
+print(len(lista))
+
+nova_lista = [1, 5, 'Ana', 'Bia']
+print(nova_lista)
+print(nova_lista.remove(5))
+print(nova_lista)
