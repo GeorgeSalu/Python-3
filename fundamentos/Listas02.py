043f05b87ebaf952f63fc6c5a6fd5d142afaac2b
George Salu <george.salu10@gmail.com>
2018-12-18 18:29:15 -0200

    listas 02

---
+++ fundamentos/Listas02.py
@@ -0,0 +1,9 @@
+lista = [1, 2, 'Rebeca', 'Guilherme', 3.145]
+
+print(lista.index('Guilherme'))
+print(lista[2])
+print(1 in lista)
+print('Rebeca' in lista)
+print('Pedro' not in lista)
+print(lista[0])
+print(lista[4])
