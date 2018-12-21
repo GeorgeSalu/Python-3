03345749f0e12b9d1a95f4eb71f57aa1d7ff415f
George Salu <george.salu10@gmail.com>
2018-12-21 20:05:19 -0200

    um pouco sobre modulos

---
+++ fundamentos_projetos/area_circulo_v5.py
@@ -0,0 +1,8 @@
+from math import pi
+
+
+raio = input('Informe o raio : ')
+print('Area do circulo ', pi * float(raio) ** 2)
+
+print('Nome do modulo', __name__)
+print('Nome do pacote', __package__)
