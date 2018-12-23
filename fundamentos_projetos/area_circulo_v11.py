8a19d22f9005b95c3751167114e914d6598560ca
George Salu <george.salu10@gmail.com>
2018-12-23 10:45:30 -0200

    melhorando o help

---
+++ fundamentos_projetos/area_circulo_v11.py
@@ -0,0 +1,22 @@
+from math import pi
+import sys
+
+
+def help():
+    print("""
+        E necessario informar o raio do circulo
+        Sintaxe: {} <raio>
+        """.format(sys.argv[0]))
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+    else:
+        raio = sys.argv[1]
+        area = circulo(raio)
+        print('Area do circulo ', area)
