6b4e3ec617999e6686c24dbb11afd08f39a875a1
George Salu <george.salu10@gmail.com>
2018-12-23 14:25:13 -0200

    for 04

---
+++ estrutura_controle/for_4.py
@@ -0,0 +1,23 @@
+# for i in range(1, 10):
+#     if i == 6:
+#         break
+#     print(i)
+# else:
+#     print('Fim')
+
+from random import randint
+
+
+def sortear_dado():
+    return randint(1, 6)
+
+
+for i in range(1, 7):
+    if i % 2 == 1:
+        continue
+
+    if sortear_dado() == i:
+        print('acertou ', i)
+        break
+else:
+    print('nao acertou o numero')
