062b5b70fae7ed45ff2b6752c95f23663b6ce22a
George Salu <george.salu10@gmail.com>
2018-12-23 11:05:22 -0200

    Validando argumentos Numerico

---
+++ fundamentos_projetos/nota_aluno.py
@@ -0,0 +1,12 @@
+nota = 9
+
+if nota >= 9:
+    print("quadro de honra")
+elif nota >= 7:
+    print("Aprovado")
+elif nota >= 5:
+    print("Recuperacao")
+elif nota >= 3:
+    print("Recuperacao + trabalho")
+else:
+    print("reprovado")
