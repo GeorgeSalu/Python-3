b98b6d1813b8f56dc24841b92d44c03c018cfd33
George Salu <george.salu10@gmail.com>
2018-12-29 12:55:12 -0200

    parametros nomeados

--- funcoes/gerador_html_v1.py
+++ funcoes/gerador_html_v1.py
@@ -4,5 +4,8 @@ def tag_bloco(texto, classe='success'):
 
 if __name__ == '__main__':
     # teste (assertions)
-    assert tag_bloco('Incluindo com sucesso') == \
+    assert tag_bloco('Incluido com sucesso') == \
         '<div class="success">Incluido com sucesso</div>'
+    assert tag_bloco('Impossivel excluir', 'erro') == \
+        '<div class="erro">Impossivel excluir</div>'
+    print(tag_bloco('bloco'))
