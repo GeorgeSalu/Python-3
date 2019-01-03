caeadff1d9192cdcdd1c7c5663e46038b4bf7ea4
George Salu <george.salu10@gmail.com>
2019-01-03 19:28:22 -0200

    implementando o vencimento

---
+++ poo/todo_v4.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timedelta
+
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def __iter__(self):
+        return self.tarefas.__iter__()
+
+    def add(self, descricao, vencimento=None):
+        self.tarefas.append(Tarefa(descricao, vencimento))
+
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito]
+
+    def procurar(self, descricao):
+        # Possivel IndexErro
+        return [tarefa for tarefa in self.tarefas
+                if tarefa.descricao == descricao][0]
+
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())}) tarefa(s) pendente(s)'
+
+
+class Tarefa:
+    def __init__(self, descricao, vencimento=None):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+        self.vencimento = vencimento
+
+    def concluir(self):
+        self.feito = True
+
+    def __str__(self):
+        status = []
+        if self.feito:
+            status.append('(concluida)')
+        elif self.vencimento:
+            if datetime.now() > self.vencimento:
+                status.append('(vencida)')
+            else:
+                dias = (self.vencimento - datetime.now()).days
+                status.append(f'(vence em {dias} dias)')
+
+        return f'{self.descricao} '+' '.join(status)
+
+
+def main():
+    casa = Projeto('Tarefa de casa')
+    casa.add('Passar roupa')
+    casa.add('Lavar prato', datetime.now() + timedelta(days=3, minutes=12))
+    print(casa)
+
+    casa.procurar('Lavar prato').concluir()
+    for tarefa in casa:
+        print(f'- {tarefa}')
+    print(casa)
+
+    mercado = Projeto('Compra no mercado')
+    mercado.add('Frutas secas')
+    mercado.add('carne')
+    mercado.add('Tomate', datetime.now() + timedelta(days=3, minutes=12))
+    print(mercado)
+
+    comprar_carne = mercado.procurar('Carne')
+    comprar_carne.concluir()
+
+
+if __name__ == '__main__':
+    main()
