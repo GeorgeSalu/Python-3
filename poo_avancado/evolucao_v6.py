7a5a3cc4973ba4852b275faa93eb99cc062eee35
George Salu <george.salu10@gmail.com>
2019-01-06 13:48:06 -0200

    classe abstrata 02

---
+++ poo_avancado/evolucao_v6.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta, abstractproperty
+
+
+class Humano(metaclass=ABCMeta):
+    # atributo da classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+        self._idade = None
+
+    @abstractproperty
+    def inteligente(self):
+        raise NotImplementedError('Propriedade nao implementada')
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.setter
+    def idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade deve ser um numero positivo')
+        self._idade = idade
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilitis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Australopitecos',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-2]
+
+    @property
+    def inteligente(self):
+        return False
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+    @property
+    def inteligente(self):
+        return False
